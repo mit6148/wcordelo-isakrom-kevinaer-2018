59540c85f43cfdf19da6ef261057c01ee2c9fb2f
Kevin Escobar Rodriguez <kevinaer@mit.edu>
2018-01-30 06:04:45 -0500

    editing orgs

--- src/main/java/io/smartraise/controller/view/OrganizationViewController.java
+++ src/main/java/io/smartraise/controller/view/OrganizationViewController.java
@@ -1,8 +1,10 @@
 package io.smartraise.controller.view;
 
+import io.smartraise.model.Image;
+import io.smartraise.model.accounts.Member;
+import io.smartraise.model.fundraise.Event;
 import io.smartraise.model.fundraise.Organization;
-import io.smartraise.service.MemberService;
-import io.smartraise.service.OrganizationService;
+import io.smartraise.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,10 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class OrganizationViewController {
@@ -21,6 +27,15 @@ public class OrganizationViewController {
 
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Autowired
+    private DonationService donationService;
+
+    @Autowired
+    private EventService eventService;
 
     @GetMapping("/organization")
     public String getOrganizationForm(Model model, Principal principal, HttpServletResponse response){
@@ -52,7 +67,26 @@ public class OrganizationViewController {
                                       Model model, Principal principal, HttpServletResponse response){
 //        if (principal != null && !principal.getName().isEmpty()) {
             Organization organization = organizationService.get(id);
+            Map<String, String> member_images = new HashMap<>();
+            List<List<Member>> lists = new ArrayList<>();
+            List<Member> listOfThree = new ArrayList<>();
+            for (Member member: memberService.getMembersFromOrganization(organization)) {
+                listOfThree.add(member);
+                if (listOfThree.size() == 3) {
+                    lists.add(listOfThree);
+                    listOfThree = new ArrayList<>();
+                }
+                try {
+                    member_images.put(member.getUsername(), imageService.get(member.getUsername(), Image.ImageType.PROFILE));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
             model.addAttribute("organization",organization);
+            model.addAttribute("members",lists);
+            model.addAttribute("member_images",member_images);
+            model.addAttribute("events",
+                    eventService.getCurrentEventsFromOrganization(organization.getOrganizationId()));
             return "organization";
 //        } else {
 //            return "login";
